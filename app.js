ff7dd43723e027504c02d0b49ae29382797cfefc
Pefes <pawlo898@gmail.com>
2019-10-28 16:08:29 +0000

    init project

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require("express"),
+	app = express(),
+	indexRoutes = require("./routes/index"),
+	roomsRoutes = require("./routes/rooms"),
+	reservationsRoutes = require("./routes/reservations"),
+	usersRoutes = require("./routes/users");
+
+
+app.use(indexRoutes);
+app.use(roomsRoutes);
+app.use(reservationsRoutes);
+app.use(usersRoutes);
+
+
+
+app.listen(3000, function(){
+	console.log("Server is listening on port: 3000");
+});
