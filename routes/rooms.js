22c890546ab0eae81f0cfabe514c94cb0941d6d1
Pefes <pawlo898@gmail.com>
2019-10-31 23:49:16 +0100

    add basic show index pages for rooms

--- routes/rooms.js
+++ routes/rooms.js
@@ -1,12 +1,44 @@
 var express = require("express"),
-	router = express.Router();
+	router = express.Router(),
+	request = require("request");
+
+//API URLs	
+var getAllRoomsUrl = {
+	url: "http://localhost:8080/rooms",
+	json: true
+	},
+	getRoomByIdUrl = {
+		url: "http://localhost:8080/rooms/",
+		json: true
+	};
+
 
 router.get("/rooms", function(req, res){
-	res.render("rooms/rooms.ejs");
+	request(getAllRoomsUrl, function(err, response, json){
+		if(err){
+			res.send("Something went wrong! :(");
+		}
+		else{
+			res.render("rooms/index.ejs", {rooms: json});	
+		}
+	});
 });
 
 router.get("/rooms/new", function(req, res){
-	res.render("rooms/newRoom.ejs");
+	res.render("rooms/new.ejs");
+});
+
+router.get("/rooms/:id", function(req, res){
+	var tempUrl = getRoomByIdUrl;
+	tempUrl.url = tempUrl.url + req.params.id;
+	request(getRoomByIdUrl, function(err, response, json){
+		if(err){
+			res.send("Something went wrong! :(");
+		}
+		else{
+			res.render("rooms/show.ejs", {room: json});
+		}
+	});
 });
 
 module.exports = router;
